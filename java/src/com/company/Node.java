c455feb24c4e228f60c0ccd008ff858926d43de4
jitendra <jitendrakumar89973@gmail.com>
2020-12-17 10:16:30 +0530

    linkedlist all function created program

---
+++ java/src/com/company/Node.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Node {
+    int data;
+    Node next;
+
+    // write your code here
+    public Node(int item) {
+        this.data = item;
+        this.next = null;
+    }
+}
