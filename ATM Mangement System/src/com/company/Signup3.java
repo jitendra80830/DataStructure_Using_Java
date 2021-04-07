087a5f6388a673311452158ca2e5dc42cdae6803
jitendra <jitendrakumar89973@gmail.com>
2021-04-07 15:45:36 +0530

    Creating a project

--- ATM Mangement System/src/com/company/Signup3.java
+++ ATM Mangement System/src/com/company/Signup3.java
@@ -2,12 +2,15 @@ package com.company;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Signup3 extends JFrame {
+public class Signup3 extends JFrame implements ActionListener {
     JLabel l1,l2,l3,l4,l5,l6,l7,l8,l9,l10,l11;
     JRadioButton r1,r2,r3,r4;
     JButton b1,b2;
     JCheckBox c1,c2,c3,c4,c5,c6,c7;
+    JTextField t1;
 
 
     Signup3(){
@@ -94,6 +97,125 @@ public class Signup3 extends JFrame {
         c7 = new JCheckBox("I hereby declare that above entered details correct to the best of my knowledge.",true);
         c7.setFont(new Font("Raleway",Font.BOLD,12));
         c7.setBackground(Color.WHITE);
+
+        r1 = new JRadioButton("Saving Account");
+        r1.setFont(new Font("Raleway",Font.BOLD,16));
+        r1.setBackground(Color.WHITE);
+
+        r2 = new JRadioButton("Fixed Deposit  Account");
+        r2.setFont(new Font("Raleway",Font.BOLD,16));
+        r2.setBackground(Color.WHITE);
+
+        r3 = new JRadioButton("Current Account");
+        r3.setFont(new Font("Raleway",Font.BOLD,16));
+        r3.setBackground(Color.WHITE);
+
+        r4 = new JRadioButton("Recurring Deposit Account");
+        r4.setFont(new Font("Raleway",Font.BOLD,16));
+        r4.setBackground(Color.WHITE);
+
+        t1 = new JTextField();
+        t1.setFont(new Font("Raleway",Font.BOLD,12));
+
+
+        setLayout(null);
+
+        l11.setBounds(700,10,70,30);
+        add(l11);
+
+        t1.setBounds(770,10,40,30);
+        add(t1);
+
+        l1.setBounds(280,50,400,40);
+        add(l1);
+
+        l2.setBounds(100,140,200,30);
+        add(l2);
+
+        r1.setBounds(100,180,150,30);
+        add(r1);
+
+        r2.setBounds(350,180,300,30);
+        add(r2);
+
+        r3.setBounds(100,220,250,30);
+        add(r3);
+
+        r4.setBounds(350,220,250,30);
+        add(r4);
+
+        l3.setBounds(100,300,200,30);
+        add(l3);
+
+        l4.setBounds(330,300,250,30);
+        add(l4);
+
+        l5.setBounds(100,330,200,20);
+        add(l5);
+
+        l6.setBounds(330,330,500,20);
+        add(l6);
+
+        l7.setBounds(100,370,200,30);
+        add(l7);
+
+        l8.setBounds(330,370,200,30);
+        add(l8);
+
+        l9.setBounds(100,400,200,20);
+        add(l9);
+
+        l10.setBounds(100,540,200,30);
+        add(l10);
+
+        c1.setBounds(100,500,200,30);
+        add(c1);
+
+        c2.setBounds(350,500,200,30);
+        add(c2);
+
+        c3.setBounds(100,550,200,30);
+        add(c3);
+
+        c4.setBounds(350,550,200,30);
+        add(c4);
+
+        c5.setBounds(100,600,200,30);
+        add(c5);
+
+        c6.setBounds(350,600,200,30);
+        add(c6);
+
+        c7.setBounds(100,680,600,20);
+        add(c7);
+
+        b1.setBounds(300,720,100,30);
+        add(b1);
+
+        b2.setBounds(420,720,100,30);
+        add(b2);
+
+        getContentPane().setBackground(Color.WHITE);
+        setSize(830,830);
+        setLocation(500,90);
+        setVisible(true);
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+
+
+    }
+    public void actionPerformed(ActionEvent ae){
+        String a = null;
+        if(r1.isSelected()){
+            a = "Saving Account";
+        }else if(r2.isSelected()){
+            a = "Fixed Deposit Account";
+        }else if(r3.isSelected()){
+            a = "Current Account";
+        }else if(r4.isSelected()){
+            a = "Recurring Deposit Account";
+        }
 
 
     }
