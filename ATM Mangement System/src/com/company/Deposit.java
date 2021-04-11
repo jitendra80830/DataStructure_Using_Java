4407355f652768914c7cc15d79888dad2b70716c
jitendra <jitendrakumar89973@gmail.com>
2021-04-11 15:30:44 +0530

    Creating A Project

---
+++ ATM Mangement System/src/com/company/Deposit.java
@@ -0,0 +1,104 @@
+package com.company;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.sql.*;
+import java.util.*;
+
+public class Deposit extends JFrame implements ActionListener{
+    JTextField t1,t2;
+    JButton b1,b2,b3;
+    JLabel l1,l2,l3;
+
+
+    Deposit(){
+
+
+        setFont(new Font("System",Font.BOLD,22));
+        Font f = getFont();
+        FontMetrics fm = getFontMetrics(f);
+        int x = fm.stringWidth("Deposit");
+        int y = fm.stringWidth(" ");
+        int z = getWidth() - x;
+        int w = z/y;
+        String pad = "";
+        pad = String.format("%"+w+"s",pad);
+        setTitle(pad+"Deposit");
+
+        l1 = new JLabel("Enter Amount you want");
+        l1.setFont(new Font("System",Font.BOLD,35));
+
+        l2 = new JLabel("To Deposit");
+        l2.setFont(new Font("System",Font.BOLD,35));
+
+        l3 = new JLabel("Enter Pin");
+        l3.setFont(new Font("System",Font.BOLD,14));
+
+        t1 = new JTextField();
+        t1.setFont(new Font("Raleway",Font.BOLD,22));
+
+        t2 = new JTextField();
+        t2.setFont(new Font("Raleway",Font.BOLD,14));
+
+        b1 = new JButton("DEPOSIT");
+        b1.setFont(new Font("System",Font.BOLD,18));
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
+
+        b2 = new JButton("BACK");
+        b2.setFont(new Font("System",Font.BOLD,18));
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
+
+        b3 = new JButton("EXIT");
+        b3.setFont(new Font("System",Font.BOLD,18));
+        b3.setBackground(Color.BLACK);
+        b3.setForeground(Color.WHITE);
+
+        setLayout(null);
+
+        l3.setBounds(620,10,80,30);
+        add(l3);
+
+        t2.setBounds(700,10,40,30);
+        add(t2);
+
+        l1.setBounds(150,150,800,60);
+        add(l1);
+
+        l2.setBounds(290,210,800,60);
+        add(l2);
+
+        t1.setBounds(250,300,300,50);
+        add(t1);
+
+        b1.setBounds(260,380,125,50);
+        add(b1);
+
+        b2.setBounds(415,380,125,50);
+        add(b2);
+
+        b3.setBounds(300,550,200,50);
+        add(b3);
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        setSize(800,800);
+        setVisible(true);
+        setLocation(500,90);
+
+
+
+    }
+    public void actionPerformed(ActionEvent ae){
+
+    }
+    public static void main(String[] args){
+        new Deposit().setVisible(true);
+    }
+}
